0bbb9abc0eed31dbcd8339cfa3363efef24a156a
zidan2506 <duydan@metropolia.fi>
2026-09-14 15:11:54 +0300

    Import custom middleware

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const app = express();
 const tourRouter = require("./routes/tourRouter");
 const userRouter = require("./routes/userRouter");
 const { unknownEndpoint } = require("./middleware/customMiddleware");
+const { errorHandler } = require("./middleware/customMiddleware");
 const connectDB = require ('./config/db')
 
 const morgan = require("morgan");
@@ -28,7 +29,7 @@ app.use("/api/tours", tourRouter);
 app.use("/api/users", userRouter);
 
 app.use(unknownEndpoint);
-// app.use(errorHandler);
+app.use(errorHandler);
 
 const port = process.env.PORT || 4000;
 // Start the server
